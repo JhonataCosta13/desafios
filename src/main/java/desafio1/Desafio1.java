25aa70474c46426426e76d2e9c3001629ebfc069
Jhonata Costa <costajhonata8@gmail.com>
2024-02-07 20:24:40 -0300

    desafio 1 concluido

---
+++ src/main/java/desafio1/Desafio1.java
@@ -0,0 +1,16 @@
+package desafio1;
+
+import static desafio1.metodos.*;
+
+public class Desafio1 {
+
+    public static void main(String[] args){
+
+        definirNumeroMaxLinhas();
+        preencherListaNumeros();
+        mostrarEntrada();
+        ordenar();
+        montarListaFinal();
+        mostrarSaida();
+    }
+}
---
+++ src/main/java/desafio1/metodos.java
@@ -0,0 +1,69 @@
+package desafio1;
+
+import java.util.*;
+
+public class metodos {
+    private static Scanner scan = new Scanner(System.in);
+    private static Integer numeroMaxLinhas;
+    private static Integer numeroDeLinhas = 1;
+    private static List<Integer> numeros = new ArrayList<>();
+    private static List<Integer> pares = new ArrayList<>();
+    private static List<Integer> impares = new ArrayList<>();
+    private static List<Integer> listaFinal = new ArrayList<>();
+
+
+    public static void definirNumeroMaxLinhas(){
+        System.out.println("Digite o número de linhas:");
+        numeroMaxLinhas = scan.nextInt();
+        System.out.println("Digite os números:");
+    }
+
+    public static void preencherListaNumeros(){
+        while (numeroMaxLinhas >= numeroDeLinhas){
+            Integer numeroScan = scan.nextInt();
+            if(numeroScan >= 0){
+                numeros.add(numeroScan);
+                numeroDeLinhas++;
+                if(numeroScan % 2 == 0)preencherListaPares(numeroScan);
+                else preencherListaImpares(numeroScan);
+            }
+            else {
+                System.out.println("Digite apenas valores positivos!");
+            }
+        }
+    }
+
+    public static void preencherListaPares(Integer numeroScan){
+        pares.add(numeroScan);
+    }
+
+    private static void preencherListaImpares(Integer numeroScan) {
+        impares.add(numeroScan);
+    }
+
+    public static void ordenar(){
+        Collections.sort(pares);
+        Collections.sort(impares);
+    }
+
+    public static void montarListaFinal(){
+        for (int i = 0; i < pares.size(); i++){
+            listaFinal.add(pares.get(i));
+        }
+        for (int i = impares.size() - 1; i >= 0; i--){
+            listaFinal.add(impares.get(i));
+        }
+    }
+
+    public static void mostrarEntrada(){
+        System.out.println("\nOs valores digitados foram:");
+        System.out.println("Número máximo de linhas: " + numeroMaxLinhas);
+        System.out.println("Números digitados: " + numeros);
+        System.out.println("Números pares: " + pares);
+        System.out.println("Números impares: " + impares);
+    }
+
+    public static void mostrarSaida(){
+        System.out.println("Lista final: " + listaFinal);
+    }
+}
